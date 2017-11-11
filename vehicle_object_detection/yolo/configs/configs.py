71ef1c01d889f7764e02d060174d79af5d0cd4e3
Neil Nie <yongyang.nie@gmail.com>
2017-11-10 22:31:25 -0500

    keras yolo works!
    But still need to limit it to traffic stuff

---
+++ vehicle_object_detection/yolo/configs/configs.py
@@ -0,0 +1,10 @@
+import sys
+import os
+
+model_path = "./yolo_keras.h5"
+anchors_path = "./configs/yolo_keras_anchors.txt"
+classes_path = "./configs/coco_classes.txt"
+test_path = "./images"
+output_path = "./output"
+score_threshold = 0.5
+iou_threshold = 0.5
