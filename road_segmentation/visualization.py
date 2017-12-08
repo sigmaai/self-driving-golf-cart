dd46ce93d24ea8233ed91079754edddc1574ec4c
Neil Nie <yongyang.nie@gmail.com>
2017-12-08 00:05:31 -0500

    google maps & segmentation works

--- road_segmentation/visualization.py
+++ road_segmentation/visualization.py
@@ -39,8 +39,6 @@ df_truth = pd.read_csv(steering_labels, usecols=['frame_id', 'steering_angle'], 
 print("loaded dataset", len(df_truth))
 for i in range(len(df_truth)):
 
-    if i%100:
-        print("one sec")
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             break
@@ -49,14 +47,25 @@ for i in range(len(df_truth)):
 
     image = Image.open(path)
     image = np.array(image, dtype=np.uint8)
-    im_mask = model.predict(np.array([image]))[0][:,:,0]
-    im_mask = np.array(np.invert(im_mask), dtype=np.uint8)
-    im_mask = np.expand_dims(im_mask, axis=2)
-    im_mask = cv2.flip(im_mask, 0)
-    final_im = cv2.bitwise_and(image, image, mask=im_mask)
+
+    im_mask = model.predict(np.array([image]))[0][:, :, 0]
+    im_mask = np.array(255 * im_mask, dtype=np.uint8)
+    im_mask = cv2.cvtColor(im_mask, cv2.COLOR_GRAY2RGB)
+    im_mask[:, :, 1:3] = 0 * im_mask[:, :, 1:2]
+
+    # im_mask = np.expand_dims(im_mask, axis=2)
+    # im_mask = np.tile(im_mask, 3)
+    # print(im_mask.shape)
+    img_pred = cv2.addWeighted(im_mask, 0.9, image, 0.2, 0)
+
+    # im_mask = cv2.flip(im_mask, 0)
+    # final_im = cv2.bitwise_and(image, image, mask=im_mask)
 
     # show it in pygame
     # -----------------
-    pygame.surfarray.blit_array(camera_surface, final_im.swapaxes(0, 1))
+    pygame.surfarray.blit_array(camera_surface, img_pred.swapaxes(0, 1))
     screen.blit(camera_surface, (0, 0))
     pygame.display.flip()
+
+
+
