18c17bb99a49fde01c59469997ccd03c5811aa52
Neil Nie <yongyang.nie@gmail.com>
2018-04-10 15:49:26 -0400

    steering visualization changes

--- steering/steering_predictor.py
+++ steering/steering_predictor.py
@@ -1,29 +1,11 @@
 
 import numpy as np
 import cv2
-from PIL import Image
-from PIL import ImageFont
-from PIL import ImageDraw
 import steering.configs as configs
 import steering.models as models
 
 
 class SteeringPredictor:
-
-    def post_process_image(self, image, angle):
-        
-        background = Image.fromarray(np.uint8(image))
-        sw = Image.open("./steering/resources/sw.png")
-        sw = sw.rotate(angle*180/np.pi)
-        sw = sw.resize((80, 80), Image.ANTIALIAS)
-        background.paste(sw, (10, 10), sw)
-
-        draw = ImageDraw.Draw(background)
-        font = ImageFont.truetype("./steering/resources/FiraMono-Medium.otf", 16)
-        draw.text((80, 200), str(round(angle, 3)), (255, 255, 255), font=font)
-        steering_img = cv2.resize(np.array(background), (640, 480))
-        return steering_img
-
 
     def __init__(self):
 
