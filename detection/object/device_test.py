1f2179d3ca74ed7fd1470d46a47ef26f9271748f
Neil Nie <yongyang.nie@gmail.com>
2018-04-09 20:32:10 -0400

    Fixed detection 🛠 & other small stuff

--- detection/object/device_test.py
+++ detection/object/device_test.py
@@ -21,52 +21,18 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-# only run this file from this directory
+# do not run this file from this directory
+# run it from the top directory of the workspace
 
+from detection.object.object_detector import ObjectDetector
 import cv2
 import numpy as np
-import colorsys
-from keras import backend as K
-from PIL import ImageDraw, ImageFont, Image
-import model_data.configs as configs
-from keras.models import load_model
-from yad2k.models.keras_yolo import yolo_eval, yolo_head
-import random
 from imutils.video import VideoStream
 
 
 def read_cam():
 
-    sess = K.get_session()  
-
-    with open(configs.t_classes_path) as f:
-        class_names = f.readlines()
-    class_names = [c.strip() for c in class_names]
-
-    with open(configs.t_anchors_path) as f:
-        anchors = f.readline()
-        anchors = [float(x) for x in anchors.split(',')]
-        anchors = np.array(anchors).reshape(-1, 2)
-
-    print("being loading model")
-    yolo_model = load_model(configs.t_model_path)
-    yolo_model.summary()
-    print("finished loading model")
-    # Check if model is fully convolutional, assuming channel last order.
-
-    # Generate colors for drawing bounding boxes.
-    hsv_tuples = [(x / len(class_names), 1., 1.)
-                  for x in range(len(class_names))]
-    colors = list(map(lambda x: colorsys.hsv_to_rgb(*x), hsv_tuples))
-    colors = list(map(lambda x: (int(x[0] * 255), int(x[1] * 255), int(x[2] * 255)), colors))
-    random.seed(10101)      # Fixed seed for consistent colors across runs.
-    random.shuffle(colors)  # Shuffle colors to decorrelate adjacent classes.
-    random.seed(None)       # Reset seed to default.
-
-    # Generate output tensor targets for filtered bounding boxes.
-    yolo_outputs = yolo_head(yolo_model.output, anchors, len(class_names))
-    input_image_shape = K.placeholder(shape=(2, ))
-    boxes, scores, classes = yolo_eval(yolo_outputs, input_image_shape, score_threshold=configs.score_threshold, iou_threshold=configs.iou_threshold)
+    detector = ObjectDetector()
 
     # OpenCV main loop
     vid_center = VideoStream(src=0).start()
@@ -91,58 +57,9 @@ def read_cam():
             # -----------------------------------------------------
             # run the network and detection
 
-            resized_image = cv2.resize(image, (416, 416))
+            result = detector.detect_object(image)
 
-            image_data = np.array(resized_image, dtype='float32')
-
-            image_data /= 255.
-            image_data = np.expand_dims(image_data, 0)  # Add batch dimension.
-
-            # making predictions
-            out_boxes, out_scores, out_classes = sess.run(
-                [boxes, scores, classes],
-                feed_dict={
-                    yolo_model.input: image_data,
-                    input_image_shape: [image.shape[1], image.shape[0]],
-                    K.learning_phase(): 0
-                })
-
-            font = ImageFont.truetype(font='font/FiraMono-Medium.otf',
-                                      size=np.floor(3e-2 * image.shape[1] + 0.5).astype('int32'))
-            thickness = (image.shape[0] + image.shape[1]) // 300
-
-            array = np.uint8((image))
-            image = Image.fromarray(array)
-
-            # draw the bounding boxes
-            for i, c in reversed(list(enumerate(out_classes))):
-                predicted_class = class_names[c]
-                box = out_boxes[i]
-                score = out_scores[i]
-
-                label = '{} {:.2f}'.format(predicted_class, score)
-                draw = ImageDraw.Draw(image)
-                label_size = draw.textsize(label, font)
-
-                top, left, bottom, right = box
-                top = max(0, np.floor(top + 0.5).astype('int32'))
-                left = max(0, np.floor(left + 0.5).astype('int32'))
-                bottom = min(image.size[1], np.floor(bottom + 0.5).astype('int32'))
-                right = min(image.size[0], np.floor(right + 0.5).astype('int32'))
-
-                if top - label_size[1] >= 0:
-                    text_origin = np.array([left, top - label_size[1]])
-                else:
-                    text_origin = np.array([left, top + 1])
-
-                # My kingdom for a good redistributable image drawing library.
-                for i in range(thickness):
-                    draw.rectangle([left + i, top + i, right - i, bottom - i], outline=colors[c])
-                draw.rectangle([tuple(text_origin), tuple(text_origin + label_size)], fill=colors[c])
-                draw.text(text_origin, label, fill=(0, 0, 0), font=font)
-                del draw
-
-            displayBuf = np.array(image)
+            displayBuf = np.array(result)
 
             # show the stuff
             # -----------------------------------------------------
