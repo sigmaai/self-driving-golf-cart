a4c01693746ffdd2180e9c3cf67307f2120d8dbe
Neil Nie <yongyang.nie@gmail.com>
2018-04-09 20:32:10 -0400

    Fixed detection 🛠 & other small stuff

--- detection/object/model_data/configs.py
+++ detection/object/model_data/configs.py
@@ -4,9 +4,10 @@
 # All Rights Reserved.
 # -----------------------------------
 
-model_path = "./detection/object/yolo.h5"
+model_path = "./detection/object/model_data/yolo.h5"
 anchors_path = "./detection/object/model_data/yolo_anchors.txt"
-classes_path = "./detection/object/model_data/pascal_classes.txt"
+classes_path = "./detection/object/model_data/coco_classes.txt"
+
 t_model_path = "./yolo.h5"
 t_anchors_path = "./model_data/yolo_anchors.txt"
 t_classes_path = "./model_data/pascal_classes.txt"
@@ -14,5 +15,5 @@ t_classes_path = "./model_data/pascal_classes.txt"
 score_threshold = 0.5
 iou_threshold = 0.5
 test_dataset = "/Volumes/Personal_Drive/Datasets/udacity-driving/validation/"
-width = 480
-height = 640
+width = 416
+height = 416
--- detection/object/object_detector.py
+++ detection/object/object_detector.py
@@ -1,16 +1,39 @@
-#
-# Vehicle detection class
-# Self-driving golf cart
-# Developed by Neil Nie | (c) 2018
-#
+#! /usr/bin/env python3
 
-import cv2
-import random
+# MIT License
+
+# Copyright (c) 2017 Yongyang Nie
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+"""Run a YOLO_v2 style detection model test images."""
+
+
 import colorsys
+import cv2
+import os
+import random
+
 import numpy as np
-from keras.models import load_model
-from PIL import ImageDraw, ImageFont, Image
 from keras import backend as K
+from keras.models import load_model
+from PIL import Image, ImageDraw, ImageFont
 import detection.object.model_data.configs as configs
 from detection.object.yad2k.models.keras_yolo import yolo_eval, yolo_head
 
@@ -18,6 +41,9 @@ from detection.object.yad2k.models.keras_yolo import yolo_eval, yolo_head
 class ObjectDetector:
 
     def __init__(self):
+
+        model_path = os.path.expanduser(configs.model_path)
+        assert model_path.endswith('.h5'), 'Keras model must be a .h5 file.'
 
         self.sess = K.get_session()
 
@@ -30,48 +56,53 @@ class ObjectDetector:
             anchors = [float(x) for x in anchors.split(',')]
             anchors = np.array(anchors).reshape(-1, 2)
 
-        self.yolo_model = load_model(configs.model_path)
-        self.yolo_model.summary()
-
-        model_image_size = self.yolo_model.layers[0].input_shape
-        is_fixed_size = model_image_size != (None, None)
-        print(model_image_size)
+        self.yolo_model = load_model(model_path)
+        print(self.yolo_model.summary())
+        self.model_image_size = self.yolo_model.layers[0].input_shape[1:3]
+        print('{} model, anchors, and classes loaded.'.format(model_path))
 
         # Generate colors for drawing bounding boxes.
-        hsv_tuples = [(x / len(class_names), 1., 1.) for x in range(len(class_names))]
+        hsv_tuples = [(x / len(class_names), 1., 1.)
+                      for x in range(len(class_names))]
         self.colors = list(map(lambda x: colorsys.hsv_to_rgb(*x), hsv_tuples))
-        self.colors = list(map(lambda x: (int(x[0] * 255), int(x[1] * 255), int(x[2] * 255)), self.colors))
+        self.colors = list(
+            map(lambda x: (int(x[0] * 255), int(x[1] * 255), int(x[2] * 255)),
+                self.colors))
         random.seed(10101)  # Fixed seed for consistent colors across runs.
         random.shuffle(self.colors)  # Shuffle colors to decorrelate adjacent classes.
         random.seed(None)  # Reset seed to default.
 
-        self.font = ImageFont.truetype(font='detection/object/font/FiraMono-Medium.otf',
-                                  size=np.floor(3e-2 * configs.height + 0.5).astype('int32'))
-        self.thickness = (configs.width + configs.height) // 300
-
         # Generate output tensor targets for filtered bounding boxes.
         yolo_outputs = yolo_head(self.yolo_model.output, anchors, len(class_names))
         self.input_image_shape = K.placeholder(shape=(2,))
-        self.boxes, self.scores, self.classes = yolo_eval(yolo_outputs, self.input_image_shape, score_threshold=configs.score_threshold,
-                                           iou_threshold=configs.iou_threshold)
+        self.boxes, self.scores, self.classes = yolo_eval(
+            yolo_outputs,
+            self.input_image_shape,
+            score_threshold=configs.score_threshold,
+            iou_threshold=configs.iou_threshold)
 
-    def detect_objects(self, image, details=False):
+        # Graphics of stuff
+        self.font = ImageFont.truetype(font='./detection/object/font/FiraMono-Medium.otf',
+                                       size=np.floor(3e-2 * configs.height + 0.5).astype('int32'))
+        self.thickness = (configs.width + configs.height) // 300
 
-        # don't need to resize image
-        # when calling the method
+    def detect_object(self, image, details=False):
 
-        resized_image = cv2.resize(image, (416, 416))
+        image = Image.fromarray(cv2.resize(image, (configs.width, configs.height)))
+
+        resized_image = image.resize(
+            tuple(reversed(self.model_image_size)), Image.BICUBIC)
         image_data = np.array(resized_image, dtype='float32')
 
         image_data /= 255.
-        # image_data = np.expand_dims(, 0)  # Add batch dimension.
+        image_data = np.expand_dims(image_data, 0)  # Add batch dimension.
         print(image_data.shape)
-        # making predictions
+
         out_boxes, out_scores, out_classes = self.sess.run(
             [self.boxes, self.scores, self.classes],
             feed_dict={
-                self.yolo_model.input: np.array([image_data]),
-                self.input_image_shape: [image.shape[1], image.shape[0]],
+                self.yolo_model.input: image_data,
+                self.input_image_shape: [image.size[1], image.size[0]],
                 K.learning_phase(): 0
             })
 
