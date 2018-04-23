5b2bbb6639534e27f5105a6b0156d27c4d1f05c5
Neil Nie <yongyang.nie@gmail.com>
2018-04-23 12:38:10 -0400

    ros branch initial commit

--- logger/build/gtest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/gmock
-# Build directory: /home/yongyang/Developer/self-driving-golf-cart/logger/build/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
---
+++ src/localization/imu.py
@@ -0,0 +1,5 @@
+#	
+#	imu.py | code for the imu unit
+#	for the golf cart
+#	(c) Yongyang (Neil) Nie, 2018
+#
