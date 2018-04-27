471f33d6b8aca0b4b7cf9b14689f084bdf610d81
Neil Nie <yongyang.nie@gmail.com>
2018-04-27 16:23:16 -0400

    rviz added. vehicle mesh added. other small launch file changes

--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -7,6 +7,4 @@
 # new environment variables
 
 # modified environment variables
-export LD_LIBRARY_PATH="/opt/ros/kinetic/lib:/opt/ros/kinetic/lib/x86_64-linux-gnu:/usr/local/cuda-9.0/lib64"
-export PKG_CONFIG_PATH="/opt/ros/kinetic/lib/pkgconfig:/opt/ros/kinetic/lib/x86_64-linux-gnu/pkgconfig"
 export PWD="/home/neil/Workspace/self-driving-golf-cart/build"
