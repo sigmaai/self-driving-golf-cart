c8a73cd6a3dae94985af9a29614a7bc531c6257e
Neil Nie <yongyang.nie@gmail.com>
2018-05-07 17:49:33 -0400

    gps localization with rviz finally worksgit status

--- src/localization/scripts/gps_localization.py
+++ src/localization/scripts/gps_localization.py
@@ -12,7 +12,6 @@ import geodesy.props
 import geodesy.utm
 import geodesy.wu_point
 import geodesy
-from std_msgs.msg import Float32
 from sensor_msgs.msg import NavSatFix
 import math
 
@@ -23,34 +22,39 @@ class GPSLocalization():
 
         rospy.init_node('gps_localization')
         self.br = tf.TransformBroadcaster()
-        self.rate = rospy.Rate(10.0)
 
-        self.lat = 0
-        self.long = 0
+        self.x = 0
+        self.y = 0
 
         rospy.Subscriber('/sensor/gps/fix', NavSatFix, callback=self.navsatfix_callback, queue_size=5)
-        rospy.spin()
+
+        self.rate = rospy.Rate(30.0)
 
         while not rospy.is_shutdown():
-            self.br.sendTransform((self.lat - 696400.1, self.long - 4713254.8, 0),
+            self.br.sendTransform((self.x, self.y, 0),
                              (0, 0, 0, 1),
                              rospy.Time.now(),
                              "base_link",
                              "local_map")
             self.rate.sleep()
 
-        #
         # 1. use geodesy to convert LatLong to point
         # 2. subtract the UTM static transformation from it
         # 3. send that transformation
 
     def navsatfix_callback(self, msg):
 
-        point = geodesy.utm.fromLatLong(msg.longitude, msg.latitude).toPoint()
-        point.x = point.x - 696400.1
-        point.y = point.y - 4713254.8
-        self.lat = point.x
-        self.long = point.y
+        if msg.status.status == -1:
+            print("no data received")
+            
+        else:
+            # point = geodesy.utm.fromLatLong(42.546895, -72.609342).toPoint()
+            point = geodesy.utm.fromLatLong(msg.latitude, msg.longitude).toPoint()
+            self.x = float(point.x) - 696400.1
+            self.y = float(point.y) - 4713254.8
+            print(self.x)
+            print(self.y)
+            
 
 
 if __name__ == '__main__':
