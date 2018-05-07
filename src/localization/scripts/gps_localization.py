1c3891020aaa74d3962675cb1caee87ff41d4b6f
Neil Nie <yongyang.nie@gmail.com>
2018-05-07 16:52:09 -0400

    basic gps localization implementations

--- src/localization/scripts/gps_localization.py
+++ src/localization/scripts/gps_localization.py
@@ -17,35 +17,48 @@ from sensor_msgs.msg import NavSatFix
 import math
 
 
-def navsatfix_callback(msg):
+class GPSLocalization():
 
-    point = geodesy.utm.fromLatLong(msg.longitude, msg.latitude).toPoint()
-    point.x = point.x - 696400.1
-    point.y = point.y - 4713254.8
+    def __init__(self):
 
-    while not rospy.is_shutdown():
-        br.sendTransform((point.x - 696400.1, point.y - 4713254.8, 0),
-                         (0, 0, 0, 1),
-                         rospy.Time.now(),
-                         "base_link",
-                         "local_map")
-        rate.sleep()
+        rospy.init_node('gps_localization')
+        self.br = tf.TransformBroadcaster()
+        self.rate = rospy.Rate(10.0)
+
+        self.lat = 0
+        self.long = 0
+
+        rospy.Subscriber('/sensor/gps/fix', NavSatFix, callback=self.navsatfix_callback, queue_size=5)
+        rospy.spin()
+
+        while not rospy.is_shutdown():
+            self.br.sendTransform((self.lat - 696400.1, self.long - 4713254.8, 0),
+                             (0, 0, 0, 1),
+                             rospy.Time.now(),
+                             "base_link",
+                             "local_map")
+            self.rate.sleep()
+
+        #
+        # 1. use geodesy to convert LatLong to point
+        # 2. subtract the UTM static transformation from it
+        # 3. send that transformation
+
+    def navsatfix_callback(self, msg):
+
+        point = geodesy.utm.fromLatLong(msg.longitude, msg.latitude).toPoint()
+        point.x = point.x - 696400.1
+        point.y = point.y - 4713254.8
+        self.lat = point.x
+        self.long = point.y
+
 
 if __name__ == '__main__':
 
-    rospy.init_node('gps_localization')
-    br = tf.TransformBroadcaster()
-    rate = rospy.Rate(10.0)
-
-    rospy.Subscriber('/sensor/gps/fix', NavSatFix, callback=navsatfix_callback, queue_size=5)
-    rospy.spin()
-
-    #
-    # 1. use geodesy to convert LatLong to point
-    # 2. subtract the UTM static transformation from it
-    # 3. send that transformation
-    #
-    #
+    try:
+        GPSLocalization()
+    except rospy.ROSInterruptException:
+        pass
 
 
 
