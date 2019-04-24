0b6db48caf024748bc886c1fdcf80f0ad1a9810a
Neil Nie <yongyang.nie@gmail.com>
2019-04-24 19:40:52 -0400

    able to plan and execute path
    - the vehicle was able to plan out a global path, and local paths
    - added motion control package. subscribes to cmd_vel and publishes steering cmds.
    - attempt to optimize parameters.
    - there might be bugs after parameter optimization
    
    Very promising progress today! More mapping.

--- ros/src/cmake-build-debug/catkin_generated/setup_cached.sh
+++ ros/src/cmake-build-debug/catkin_generated/setup_cached.sh
@@ -9,6 +9,7 @@
 # modified environment variables
 export CMAKE_PREFIX_PATH="/home/neil/Workspace/self-driving-golf-cart/ros/src/cmake-build-debug/devel:$CMAKE_PREFIX_PATH"
 export LD_LIBRARY_PATH="/home/neil/Workspace/self-driving-golf-cart/ros/src/cmake-build-debug/devel/lib:$LD_LIBRARY_PATH"
+export PKG_CONFIG_PATH="/home/neil/Workspace/self-driving-golf-cart/ros/src/cmake-build-debug/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
 export PYTHONPATH="/home/neil/Workspace/self-driving-golf-cart/ros/src/cmake-build-debug/devel/lib/python2.7/dist-packages:$PYTHONPATH"
 export ROSLISP_PACKAGE_DIRECTORIES="/home/neil/Workspace/self-driving-golf-cart/ros/src/cmake-build-debug/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
 export ROS_PACKAGE_PATH="/home/neil/Workspace/self-driving-golf-cart/ros/src:$ROS_PACKAGE_PATH"
